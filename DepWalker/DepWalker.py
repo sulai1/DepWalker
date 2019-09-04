46d444d4a6a17662ff8f095b255f8c0d358ff4b9
sascha <sascha@LAPTOP-H8JPT7LB>
2019-09-05 00:38:00 +0200

    Restructioring of parsers

--- DepWalker/DepWalker.py
+++ DepWalker/DepWalker.py
@@ -1,1 +1,22 @@
-import Parser
+from  Parser import *
+import math
+
+WIDTH = 60
+
+
+parser = Procedure("begin-procedure",'end-procedure',ignore_case=True)
+fd = open('test.sqr','r')
+s = fd.read()
+#parser = Procedure()
+matches = parser.parse(s)
+
+if matches == None:
+    print('Empty')
+    
+i = 0
+for match in matches :
+	for keyword in parser.groups :
+		print("#" * (int)(WIDTH - math.floor(len(keyword) / 2)) + " " + keyword + " " + "#" * (int)(WIDTH - math.ceil(len(keyword) / 2)))
+		print(match.group(keyword))
+
+		
--- DepWalker/Parser.py
+++ DepWalker/Parser.py
@@ -1,60 +1,109 @@
-﻿from abc import ABC, abstractclassmethod
-import re
+﻿import re
 
-class Parser(ABC):
-    def __init__(self,name,regex):
-        self.regex = regex
-        self.name = name
 
-    def parse(self,string):
-        res = re.finditer(self.regex,string)
 
-        return res
+class Parser(object):
+	""" The Parser parses a code file for its components."""
+	ERROR_GRP = "err"
+	ESCAPE = "[\^$.|?*+(){}"
+	id = 0
 
-def multiline(start,end):
-    return Parser("{0}([\\s\\S]*?){1}".format(start,end))
+	def __init__(self,regex,groups=[]):
+		self.groups = groups
+		self.regex = regex
 
-identifier = r"(?P<identifier>\w*)"
-number = r"([\w.,]*)"
-parameters = r"(?P<parameters>\(([\s\S]*?)\))"
-comment = r"((!.*)\n)"
+	def parse(self,string):
+		""" Parse the string and return the matches. The matches can be searched for the groups stored in the parser """
+		res = re.finditer(self.regex,string)
+		return res
+	
+	@staticmethod
+	def dup_grp(name):
+		Parser.id += 1
+		return "{0}{1}".format(name,Parser.id) 
+		
+	@staticmethod
+	def error_grp():
+		Parser.id += 1
+		return "{0}{1}".format(Parser.ERROR_GRP,Parser.id) 
+		
+	@staticmethod
+	def group_name(name):
+		newname = ""
+		for c in name :
+			if c.isalnum() :
+				newname+=c
+			else :
+				newname+=""
+		return newname
 
-def Procedure():
-    return Parser("Procedure",r"(?i)(?P<comment>{2}*)begin-procedure[\s]+{0}[\s]*{1}([\s\S]*?)end-procedure".format(identifier,parameters,comment))
+	def __add__(self,other):
+		g = self.groups
+		while  self.rename_dups(other):
+			pass
+		g.extend(other.groups)
+		return Parser(self.regex + other.regex,g)
 
+	def rename_dups(self,other):
+		b_dups = False
+		for gs in self.groups :
+			for i in range(0, len(other.groups)) :
+				if gs == other.groups[i] :
+					other.regex = other.regex.replace(other.groups[i],other.groups[i] + "I")
+					other.groups[i]+="I"
+					b_dups = True
+		return b_dups
+
+def Identifier(ignore_case=False):
+	return Parser(r"(?P<identifier>\w*)", groups=["identifier"])
+
+def Keyword(keyword, ignore_case=False):
+	grpname = Parser.group_name(keyword)
+	kw=""
+	for c in keyword :
+		if Parser.ESCAPE.find(c)>0:
+			kw+=r"\{0}".format(c)
+	if len(grpname) == 0:
+		grpname = Parser.error_grp()
+	if ignore_case :
+		regex = r"(?P<{0}>".format(grpname)
+		for c in kw :
+			if Parser.ESCAPE.find(c)>0:
+				regex+=r"[\{0}]".format(c)
+			elif c.islower() :
+				regex += r"[{0}{1}]".format(c,c.upper())
+			elif c.isupper :
+				regex += r"[{0}{1}]".format(c,c.lower())
+			else :
+				regex+=r"[{0}]".format(c)
+		regex+=")"
+	else:
+		regex = r"(?P<{0}>{1})".format(grpname,kw)
+	return Parser(regex, groups=[grpname])
+
+def Except(string,ignore_case=False):
+	return Parser(r"(?P<content>[\s\S]*?)",groups=[Parser.group_name("content")]) + Keyword(string,ignore_case)
+
+def Multiline(start, end, ignore_case=False):
+	s = Keyword(start, ignore_case)
+	c = Except(end, ignore_case)
+	return s + c
+
+def Whitespaces():
+	return Parser("\s*")
+
+
+
+def Procedure(start,end,ignore_case=False):
+	return Keyword(start,ignore_case) + Whitespaces() + Identifier() + Whitespaces() + Multiline("(",")") + Except(end)
+	#return
+	#Parser("Procedure",r"(?i)(?P<comment>{2}*)begin-procedure[\s]+{0}[\s]*{1}([\s\S]*?)end-procedure".format(Parser.IDENTIFIEER,Parser.PARAMETERS,Parser.COMMENT,['identifier','parameters','comment']))
 def Define():
-    return Parser("Define",r"(?m)#define[\s*](\w*)[\s*]([\w,.]*[\s]*|'[\s\S]*?')")
+	return Parser("Define",r"(?m)#define[\s*](?P<define>\w*)[\s*]([\w,.]*[\s]*|'[\s\S]*?')")
 
 def Include():
-    return Parser("Include",r"(?m)#include[\s*]'([\w.]*)?'")
+	return Parser("Include",r"(?m)#include[\s*]'([\w.]*)?'")
 
 def Comment():
-    return Parser("Comment","(?P<comment>({}*))".format(comment))
-    
-
-
-fd = open('test.sqr','r')
-s = fd.read()
-parser = Comment()
-#parser = Procedure()
-matches = parser.parse(s)
-
-if matches == None:
-    print('Empty')
-    
-i = 0
-if parser.name == "Procedure":
-    for match in matches:
-        print(match.group('comment'))
-        print('procedure : {} {}'.format(match.group('identifier'),match.group('parameters')))
-        print('##############################################')
-        i = i + 1
-        
-if parser.name == "Comment":
-    for match in matches:
-        print(match.group('comment'))
-        print('##############################################')
-        i = i + 1
-
-print(i)
-
+	return Parser("Comment","(?P<comment>({}*))".format(Parserc.omment))
+	
